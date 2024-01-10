59c5e141e0257f3e55b34090fb04e3b10c1a41e4
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2024-01-09 23:03:52 -0300

    Added create lobby function

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import BlackCard from './components/BlackCard'
-import WhiteCard from './components/WhiteCard'
 import { io } from "socket.io-client"
+import Home from './components/home';
 
 const socket = io("http://localhost:3001");
 
 function App() {
 
-  useEffect(() => {
-    
-    socket.on('mensaje', (data) => {
-      console.log(data)
-
-    })
-    
-      return () => {
-        socket.off('mensaje')
-      }
-  },[])
-
-
   return (
     <>
-      <WhiteCard text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quam dicta dolorem, cumque similique'/>
-      <BlackCard text='¿Por que no puedo dormir por las noches?'/>
-      <button onClick={() => socket.emit("mensaje", "hola")}>press</button>
+      <Home socket={socket}></Home>
     </>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
 
 
-export default function Home () {
+export default function Home ({socket} : {socket : any}) {
+    const [name, setName] = useState('')
+    const [flag, setFlag] = useState(false)
+
+    useEffect(() => {
+        
+    }, [])
+
+    function handleInputChange (name : string) {
+        setName(name)
+        if (name.length > 4) {
+            setFlag(true)
+        }
+    }
+
+    function createLobby () {
+        if (flag) {
+            socket.emit('newLobby', 'Sala nueva creada de ' + name)
+        }
+    }
+
+    function searchLobbys () {
+        if (flag) {
+            socket.emit('searchLobby', '')
+        }
+    }
 
     return (
-        <></>
+        <main>
+        
+            <input placeholder="Ingrese su nombre" onChange={(e) => handleInputChange(e.target.value)}></input>
+            <button onClick={() => createLobby()}>Crear sala</button>
+            <button>Unirse a sala</button>
+        </main>
     )
 }
