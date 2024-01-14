a3847464aebc42818dd9ddc4cafccf7bd9c2f11e
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2024-01-14 18:17:12 -0300

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { io } from "socket.io-client"
 import Home from './components/home';
+import { Route, Routes } from 'react-router';
+import Lobby from './components/Lobby';
 
 const socket = io("http://localhost:3001");
 
 function App() {
 
   return (
-    <>
-      <Home socket={socket}></Home>
-    </>
+    <Routes>
+      <Route path='/' element={<Home socket={socket}/>}/>
+      <Route path='/lobby/:id' element={<Lobby/>}/>
+    </Routes>
+  
   )
 }
 
---
+++ src/components/Lobby.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+export default function Lobby () {
+    const [users, setUsers] = useState([])
+
+    return (
+        <>
+            
+        </>
+    )
+}
